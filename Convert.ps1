6555e4f29d012845be104fbfba48797648245a07
RCellar <30252277+RCellar@users.noreply.github.com>
2022-09-23 11:06:03 -0400

    Update Convert.ps1

--- Convert.ps1
+++ Convert.ps1
@@ -1,3 +1,7 @@
+[IO.File]::WriteAllBytes($B64File,[char[]][Convert]::ToBase64String([IO.File]::ReadAllBytes($SourceFile)))
+[IO.File]::WriteAllBytes($Reconstituted, [Convert]::FromBase64String([char[]][IO.File]::ReadAllBytes($B64File)))
+
+
 $EncodeSource = [Convert]::ToBase64String((Get-Content .\Test.txt -Encoding Byte))
 $GUID = New-Guid
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Test" -Name $GUID -Value $EncodeSource
