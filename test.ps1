9a5b5e0645cbd80571748561a7fc191082f47d45
RCellar <30252277+RCellar@users.noreply.github.com>
2023-06-05 10:48:24 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -53,3 +53,48 @@ foreach ($App in $Apps) {
         }
     }
 }
+
+
+# Import the SCCM module
+Import-Module ($Env:SMS_ADMIN_UI_PATH.Substring(0,$Env:SMS_ADMIN_UI_PATH.Length-5) + '\ConfigurationManager.psd1')
+
+# Set the site code and server
+$SiteCode = "<YourSiteCode>"
+$SiteServer = "<YourSiteServer>"
+
+# Connect to the SCCM site
+cd "X:\"
+Set-Location "$SiteCode`:"
+$ProviderMachine = Get-Item ($SiteCode + ':')
+
+# Get all programs with active deployments
+$programs = Get-CMProgram | Where-Object { $_.PackageID -ne $null }
+$deployments = @()
+
+foreach ($program in $programs) {
+    $deployments += Get-CMDeployment -PackageID $program.PackageID | Where-Object { $_.PackageID -ne $null }
+}
+
+# Add Windows 11 as a supported operating system if not already set
+foreach ($deployment in $deployments) {
+    $program = $programs | Where-Object { $_.PackageID -eq $deployment.PackageID }
+
+    $programProperties = $ProviderMachine.GetChildItems("SMS_Program.PackageID='$($program.PackageID)'")
+
+    # Check if the program has any supported operating systems
+    if ($programProperties.SupportedOperatingSystems -eq $null) {
+        # Add Windows 11 as a supported operating system
+        $programProperties.SupportedOperatingSystems = "Windows 11"
+        $programProperties.Put()
+        Write-Output "Added Windows 11 support to Program: $($program.Name)"
+    } else {
+        # Check if Windows 11 is already listed as a supported operating system
+        $supportedOperatingSystems = $programProperties.SupportedOperatingSystems -split ";"
+        if ($supportedOperatingSystems -notcontains "Windows 11") {
+            # Add Windows 11 as a supported operating system
+            $programProperties.SupportedOperatingSystems += ";Windows 11"
+            $programProperties.Put()
+            Write-Output "Added Windows 11 support to Program: $($program.Name)"
+        }
+    }
+}
