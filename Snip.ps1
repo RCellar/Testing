c3c7a5d0f7fa82a636ac82b3ae76342744f40af2
RCellar <30252277+RCellar@users.noreply.github.com>
2023-08-12 12:28:32 -0400

    Update Snip.ps1

--- Snip.ps1
+++ Snip.ps1
@@ -1,3 +1,12 @@
 $XPath = "*[System[EventID=102]] and *[EventData[Data[@Name='TaskName'] and (Data='\DEUtilities\Create Auth')]]"
 
 Get-WinEvent -FilterXPath $XPath -ProviderName "Microsoft-Windows-TaskScheduler"
+
+
+$XPath =@"
+    *[System[TimeCreated[@SystemTime>'$DateTime']]
+    [EventID=102 or EventID=100 or EventID=107 or EventID=110]] and *[EventData[Data[@Name='TaskName'] 
+    and (Data='\Directory\Create Auth')]]
+"@
+
+Get-WinEvent -FilterXPath $XPath -ProviderName "Microsoft-Windows-TaskScheduler" 
