2e2399fec3d54c52f9570edad71b76c7c42ebee1
RCellar <30252277+RCellar@users.noreply.github.com>
2024-04-26 20:20:04 -0400

    Update Snip.ps1

--- Snip.ps1
+++ Snip.ps1
@@ -1,18 +1,51 @@
-$XPath = "*[System[EventID=102]] and *[EventData[Data[@Name='TaskName'] and (Data='\DEUtilities\Create Auth')]]"
+Function GetLocalUsersFromUsersDir {
+    $IgnoreList = "Public,Default,f5 Pre-Logon User,defaultuser0" -split ","
+    $Path = "C:\Users"
+    $List = Get-ChildItem $Path | ? {$IgnoreList -notcontains $_.BaseName}
+    $List.BaseName
+}
 
-Get-WinEvent -FilterXPath $XPath -ProviderName "Microsoft-Windows-TaskScheduler"
+Function Get-EdgeExtensions {
+    param(
+        [Parameter(Mandatory=$true)][String]$User
+    )
 
+    $ObjList = [System.Collections.Generic.List[Object]]@()
+    $Pull = Get-ChildItem -Path "C:\Users\$($User)\AppData\Local\Microsoft\Edge\User Data\Default\Extensions" 
+    $Pull | ? {$_.BaseName -ne "Temp"} | ForEach-Object {
+        $Obj = [PSCustomObject]@{
+            ExtensionID = $_.BaseName
+        }
+        $Version = (Get-ChildItem -Path "$($_.FullName)" | Sort -Property BaseName -Descending | Select -First 1)
+        $ManifestPull = (Get-ChildItem -Path $Version.FullName -Filter manifest.json)
+        $ManifestPull = (Get-Content $ManifestPull.FullName -Raw -Encoding UTF8 | ConvertFrom-Json | Select-Object Author, homepage_url, update_url, version)
+        $ManifestPull.PSObject.Properties | ForEach {
+            $Obj | Add-Member -NotePropertyName $_.Name -NotePropertyValue $_.Value
+        }
+        try {$ExtName = EdgeExtensionLookup -Extension $Obj.ExtensionID}
+        catch {}
+        if ($ExtName) {$Obj | Add-Member -NotePropertyName Name -NotePropertyValue $ExtName}
+        #$Obj | Add-Member -NotePropertyName 'Manifest' -NotePropertyValue $ManifestPull
+        $ObjList.Add($Obj)
+    }
+    return $ObjList
+}
 
-$XPath =@"
-    *[System[TimeCreated[@SystemTime>'$DateTime']]
-    [EventID=102 or EventID=100 or EventID=107 or EventID=110]] and *[EventData[Data[@Name='TaskName'] 
-    and (Data='\Directory\Create Auth')]]
-"@
+Function EdgeExtensionLookup {
+    param(
+    [String]$Extension
+    )
 
-Get-WinEvent -FilterXPath $XPath -ProviderName "Microsoft-Windows-TaskScheduler" 
+    #Build local index cache?
 
+    try {
+        $Call = Invoke-WebRequest -URI "https://microsoftedge.microsoft.com/addons/detail/$($Extension)" -UseBasicParsing -ErrorAction Stop
+        [Void]($Call.Content -match ".*title>(?'title'.*)<\/title>")
+        $Matches.Title.Replace(" - Microsoft Edge Addons","")
+        }
+    catch {$_}
+}
 
-$Date = ((Get-Date).AddDays(-7)).ToUniversalTime()
-$Date = (Get-Date $Date -Format 'yyyy-MM-ddTHH:mm:ss.fffZ')
-$XPath = "*[System[TimeCreated[@SystemTime >= '$($Date)']]]"
-Get-WinEvent -FilterXPath $XPath -ProviderName "Microsoft-Windows-WLAN-AutoConfig"
+GetLocalUsersFromUsersDir | ForEach {
+    Get-EdgeExtensions $_
+}
